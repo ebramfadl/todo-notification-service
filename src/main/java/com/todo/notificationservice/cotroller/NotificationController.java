3a871f1a4494288d9902ea78c788e3fe5a605f05
Ebram Fadl <ebram@ntsal.com>
2025-05-01 15:37:57 +0300

    Adjusted the notification controller

--- src/main/java/com/todo/notificationservice/cotroller/NotificationController.java
+++ src/main/java/com/todo/notificationservice/cotroller/NotificationController.java
@@ -1,9 +1,17 @@
 package com.todo.notificationservice.cotroller;
 
+import com.todo.notificationservice.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping(path = "/notification")
 @RestController
 public class NotificationController {
+    @Autowired
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
 }
